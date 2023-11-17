e427ecbcbb88f8cfadef8797beecd9175393c84c
amir <amirshr13@gmail.com>
2023-11-17 18:00:19 +0330

    Stop tracking store.py

--- config.py
+++
@@ -1,9 +0,0 @@
-# PGHOST="stampy.db.elephantsql.com"
-# PGUSER="wpdhbdmn"
-# PGPASSWORD="7bCfjlViAqLBKX-zhqh6Cpmt8YHTwBgG"
-SERVER_IP = '185.82.202.245'
-PGDATABASE = "ocr_customers"
-PGHOST='185.82.202.245'
-PGUSER="king"
-PGPASSWORD="king@#1"
-
